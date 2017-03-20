aa6a8cdca68264d26cbc42dcbee4cf7dd11922f7
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-03-20 14:06:54 -0700

    started user validations

--- validations/foods.js
+++ validations/foods.js
@@ -7,6 +7,18 @@ module.exports.post = {
     food_name: Joi.string()
       .alphanum()
       .max(40)
-      .required();
+      .required(),
+    jan: Joi.boolean(),
+    feb: Joi.boolean(),
+    mar: Joi.boolean(),
+    apr: Joi.boolean(),
+    may: Joi.boolean(),
+    jun: Joi.boolean(),
+    jul: Joi.boolean(),
+    aug: Joi.boolean(),
+    sep: Joi.boolean(),
+    oct: Joi.boolean(),
+    nov: Joi.boolean(),
+    dec: Joi.boolean()
   }
 }
