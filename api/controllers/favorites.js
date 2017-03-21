f6e601842c719f3c99f74e95bd776fe8e8ae0300
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-03-20 18:35:42 -0700

    added working post route for /favorites

--- api/controllers/favorites.js
+++ api/controllers/favorites.js
@@ -15,6 +15,26 @@ function getFavorites(req, res) {
   })
 }
 
+function addFavorite(req, res) {
+  let knex = require('../../knex.js');
+  knex('favorites')
+    .insert({
+      user_id: 1,
+      recipe_id: req.body.recipe_id,
+      month: req.body.month
+    }, '*')
+    .then((favorites) => {
+      res.send(favorites[0]);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    .finally(() => {
+      // knex.destroy();
+    })
+}
+
 module.exports = {
-  getFavorites: getFavorites
+  getFavorites: getFavorites,
+  addFavorite: addFavorite
 }
