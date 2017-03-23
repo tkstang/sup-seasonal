397e84d2ddcdd91fc152973e81871bb99cc3cb47
Thomas Stang <stang.tk@gmail.com>
2017-03-22 23:29:07 -0700

    Added superuser only authorization for getUser and getAllUser routes, made improvements to swagger YAML

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -4,54 +4,69 @@ const jwt = require('jsonwebtoken');
 
 function getAllUsers(req, res, next) {
   let knex = require('../../knex.js');
-  knex('users')
-  .orderBy('id')
-  .then((users) => {
-    res.status(200).json(users);
-  })
-  .catch((err) => {
-    console.error(err);
-  })
-  .finally(() => {
-    // knex.destroy();
+  const token = req.headers['token'];
+  jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
+    if (payload.permissions !== "superuser"){
+      res.status(401).json('Unauthorized');
+    } else {
+      knex('users')
+      .orderBy('id')
+      .then((users) => {
+        res.status(200).json(users);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        // knex.destroy();
+      });
+    };
   });
 }
 
 function getUser(req, res) {
   let knex = require('../../knex.js');
   let paramId = req.swagger.params.user_id.value;
-  knex('users')
-  .where('id', paramId)
-  .then((user) => {
-    res.status(200).json(user);
-  })
-  .catch((err) => {
-    console.error(err);
-  })
-  .finally(() => {
-    // knex.destroy();
+  const token = req.headers['token'];
+  jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
+    if (payload.permissions !== "superuser"){
+      res.status(401).json('Unauthorized');
+    } else {
+      knex('users')
+      .where('id', paramId)
+      .then((user) => {
+        res.status(200).json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        // knex.destroy();
+      });
+    };
   });
 }
 
-function addUser(req, res) {
-  let knex = require('../../knex.js');
-  knex('users')
-  .insert({
-    username: req.body.username,
-    email: req.body.email,
-    permissions: req.body.permissions,
-    hashed_password: req.body.hashed_password
-  }, '*')
-  .then((user) => {
-    res.send(user[0]);
-  })
-  .catch((err) => {
-    console.error(err);
-  })
-  .finally(() => {
-    // knex.destroy();
-  });
-}
+// ----- Is there really any reason for this route? If not remove here and from YAML ----- //
+// function addUser(req, res) {
+//   let knex = require('../../knex.js');
+//   knex('users')
+//   .insert({
+//     username: req.body.username,
+//     email: req.body.email,
+//     permissions: req.body.permissions,
+//     hashed_password: req.body.hashed_password
+//   }, '*')
+//   .then((user) => {
+//     res.send(user[0]);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   })
+//   .finally(() => {
+//     // knex.destroy();
+//   });
+// }
 
 function updateUser(req, res) {
   let knex = require('../../knex.js');
